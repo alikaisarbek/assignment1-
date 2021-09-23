902f24bf0acc5d8093ab7b2c6d5e381187f2cb0e
alikaisarbek <alikaisarbek@mail.ru>
2021-09-23 13:15:53 +0600

    2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
-report = cg.get_coins_markets(vs_currency='usd')
-print(report)
+result = cg.get_coins_markets(vs_currency='usd')
+print(result)
 
-num = int(input("Enter: "))
-counter = 0
-while counter < num:
-    counter += 1
-    print(counter," - ", report[counter]['name'], report[counter]['market_cap'])
+num = int(input("Enter number: "))
+digit = 0
+while digit < num:
+    print(digit+1, " - ", result[digit]['name'], result[digit]['market_cap'])
+    digit += 1
 
 
